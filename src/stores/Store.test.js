8a11cf403474fc8bdf596256595d1a9c1518be34
CDulst <casey.dulst@gmail.com>
2020-03-05 15:18:51 +0100

    previously used lone store is now removed

--- src/stores/Store.test.js
+++
@@ -1,8 +0,0 @@
-import store from './Store';
-import User from '../models/User';
-
-
-test ('post added to store', () => {
-  store.addPost({user: new User({name: "bob", description: "eagle", pic: "assets/svg/account-icon.svg"}), picture: 'test', categorie: 'test', location: 'test', description: 'test', tags: ['test', 'test2']});
-  expect(store.posts.length).toBe(4);
-});
